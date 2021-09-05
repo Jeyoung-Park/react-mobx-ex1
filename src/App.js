aff376527478baadf7d81a71dad5ed938a073ae9
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-05 19:56:32 +0900

    bugList 에제
    https://www.youtube.com/watch?v=pnhIJA64ByY

--- src/App.js
+++ src/App.js
@@ -1,10 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import { useLocalStore, useObserver } from 'mobx-react-lite';
+import React from 'react';
+
+const StoreContext=React.createContext();
+
+// mobx store
+const StoreProvider=({children})=>{
+  const store=useLocalStore(()=>({
+    bugs:['Centipede'],
+    addBug:bug=>{
+      store.bugs.push(bug);
+    },
+    get bugsCount(){
+      return store.bugs.length;
+    }
+  }));
+
+  return(
+    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+  )
+}
+
+const BugsHeader=()=>{
+  const store=React.useContext(StoreContext);
+  return useObserver(()=><h1>{store.bugsCount} Bugs!</h1>)
+
+}
+
+const BugsList=()=>{
+  // context에 access하는 훅
+  const store=React.useContext(StoreContext);
+
+  return useObserver(()=>(
+    <ul>
+      {store.bugs.map(bug=>(
+        <li key={bug}>{bug}</li>
+      ))}
+    </ul>
+  ))
+}
+
+const BugsForm=()=>{
+  const store=React.useContext(StoreContext);
+  const [bug, setBug]=React.useState('');
+
+  return(
+    <form
+      onSubmit={e=>{
+        store.addBug(bug);
+        setBug('');
+        e.preventDefault();
+      }}
+    >
+      <input 
+        type='text'
+        value={bug}
+        onChange={e=>{
+          setBug(e.target.value)
+        }}
+      />
+      <button type='submit'>Add</button>
+    </form>
+  )
+}
 
 function App() {
   return (
-    <Counter />
+    <StoreProvider>
+      <main>
+        <BugsHeader />
+        <BugsList />
+        <BugsForm />
+      </main>
+    </StoreProvider>
   );
 }
 
