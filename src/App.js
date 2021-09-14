c634a9ee3c0f4a25200c46b5ab5e1b96a5ee4ac6
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-09-14 21:16:52 +0900

    feat: React Context를 이용해 mobx 구현

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 import ReactDOM from "react-dom";
 import { observer } from "mobx-react";
-import Counter from "./store/counter";
 import CounterStore from "./store/counter";
 
-const myCounter = new CounterStore();
+const CounterContext = createContext();
 
-const CounterView = observer(({ counter }) => (
-  <>
-    <div>{counter.count}</div>
-    <button onClick={() => counter.increase()}>+1</button>
-    <button onClick={() => counter.decrease()}>-1</button>
-    <button onClick={() => counter.reset()}>reset</button>
-  </>
-));
+const CounterView = observer(() => {
+  const counter = useContext(CounterContext);
+  return (
+    <>
+      <div>{counter.count}</div>
+      <button onClick={() => counter.increase()}>+1</button>
+      <button onClick={() => counter.decrease()}>-1</button>
+      <button onClick={() => counter.reset()}>reset</button>
+    </>
+  );
+});
 
 // ReactDOM.render(<CounterView counter={myCounter} />, document.body);
 export default function App() {
-  return <CounterView counter={myCounter} />;
+  return (
+    <CounterContext.Provider value={new CounterStore()}>
+      <CounterView />
+    </CounterContext.Provider>
+  );
 }
